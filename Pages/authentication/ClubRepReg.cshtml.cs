60ba832fe1a38385e86f1a6e2ea0f014220d2a46
omar wael <omarwael33@gmail.com>
2022-12-23 18:30:35 +0200

    done with reg

---
+++ Pages/authentication/ClubRepReg.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Sports_Management_System.Pages.authentication
+{
+    public class ClubRepRegModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ Pages/authentication/FanRegistration.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Sports_Management_System.Pages.authentication
+{
+    public class FanRegistrationModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
