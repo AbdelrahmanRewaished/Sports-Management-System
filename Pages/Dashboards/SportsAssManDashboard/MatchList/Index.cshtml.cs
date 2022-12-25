57c9ea27e3db95474ad09a409818353ff5078bc2
omar wael <omarwael33@gmail.com>
2022-12-25 17:51:08 +0200

    done with MatchList

---
+++ Pages/Dashboards/SportsAssManDashboard/MatchList/Index.cshtml.cs
@@ -0,0 +1,35 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Sports_Management_System.Models;
+
+namespace Sports_Management_System.Pages.Dashboards.SportsAssManDashboard.MatchList
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ChampionsLeagueDbContext _db;
+
+        public IndexModel(ChampionsLeagueDbContext db)
+        {
+            _db = db;
+        }
+        public List<Match> Matches { get; set; };
+
+        public async Task OnGet()
+        {
+
+            Matches = await _db.Matches.ToListAsync();
+        }
+
+        public async Task<IActionResult> OnPostDelete(int id)
+        {
+            var Match = await _db.Matches.FindAsync(id);
+            if (Match == null)
+            {
+                return NotFound();
+            }
+            _db.Matches.Remove(Match);
+            await _db.SaveChangesAsync();
+            return RedirectToPage("Index");
+        }
+    }
+}
