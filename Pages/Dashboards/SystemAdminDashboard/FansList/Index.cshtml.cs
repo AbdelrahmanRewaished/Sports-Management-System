24fd33c47c6ac8f9039b54f5d2bb31a8c0f5d8ca
AbdoRewaished <abdelrahmann.salehh@gmail.com>
2022-12-25 03:04:07 +0200

    Done with System Admin Dashboard

---
+++ Pages/Dashboards/SystemAdminDashboard/FansList/Index.cshtml.cs
@@ -0,0 +1,40 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Sports_Management_System.Models;
+
+namespace Sports_Management_System.Pages.Dashboards.SystemAdminDashboard.FansList
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ChampionsLeagueDbContext _db;
+
+        public IndexModel(ChampionsLeagueDbContext db)
+        {
+            _db = db;
+        }
+        public List<Fan> Fans { get; set; }
+        public async Task OnGet()
+        {
+            Fans = await _db.Fans.ToListAsync();
+        }
+
+        public async Task<IActionResult> OnPost(int id, bool status)
+        {
+            var Fan = await _db.Fans.FindAsync(id);
+            if (Fan == null)
+            {
+                return NotFound();
+            }
+            if(status)
+            {
+                await _db.Database.ExecuteSqlAsync($"exec blockFan {id}");
+            }
+            else
+            {
+                await _db.Database.ExecuteSqlAsync($"exec unblockFan {id}");
+            }
+            return RedirectToPage("Index");
+        }
+    }
+}
---
+++ Pages/Dashboards/SystemAdminDashboard/StadiumsList/Edit.cshtml.cs
@@ -0,0 +1,37 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Sports_Management_System.Models;
+
+namespace Sports_Management_System.Pages.Dashboards.SystemAdminDashboard.StadiumsList
+{
+    public class EditModel : PageModel
+    {
+        private readonly ChampionsLeagueDbContext _db;
+
+        public EditModel(ChampionsLeagueDbContext db)
+        {
+            _db = db;
+        }
+
+        [BindProperty]
+        public Stadium Stadium { get; set; }
+        public async Task OnGet(int id)
+        {
+            Stadium = await _db.Stadia.FindAsync(id);
+        }
+
+        public async Task<IActionResult> OnPost()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+            var StadiumFromDb = await _db.Stadia.FindAsync(Stadium.Id);
+            StadiumFromDb.Name = Stadium.Name;
+            StadiumFromDb.Location = Stadium.Location;
+            StadiumFromDb.Capacity = Stadium.Capacity;
+            await _db.SaveChangesAsync();
+            return RedirectToPage("Index");
+        }
+    }
+}
